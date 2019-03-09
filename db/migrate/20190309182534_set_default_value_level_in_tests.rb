3e713dd24f5584b5b83f630c61a7397290a1300e
Rick-ROR <Rick.ROR@ya.ru>
2019-03-09 18:31:42 +0000

    add migration set default value correct & level

---
+++ db/migrate/20190309182534_set_default_value_level_in_tests.rb
@@ -0,0 +1,5 @@
+class SetDefaultValueLevelInTests < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tests, :level, :integer, :default => 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_172601) do
+ActiveRecord::Schema.define(version: 2019_03_09_173922) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
