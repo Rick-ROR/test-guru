a58dac9ac9c97074c8d0163b6fcb550c1f36d5fd
Rick-ROR <Rick.ROR@ya.ru>
2019-03-09 18:34:08 +0000

    apply migration set default value correct & level

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_173922) do
+ActiveRecord::Schema.define(version: 2019_03_09_182534) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "correct", default: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -32,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_173922) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
