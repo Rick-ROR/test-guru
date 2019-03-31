99761cc017c90a9e5bf3d8e6bedbe2c0616c125f
Rick-ROR <Rick.ROR@ya.ru>
2019-03-31 17:35:20 +0000

    some changes

--- app/models/history_test.rb
+++ app/models/history_test.rb
@@ -3,9 +3,7 @@ class HistoryTest < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  # почему не работает before_save ?
-  before_update :before_update_next_question, on: :update
+  before_validation :before_validation_set_question, on: [:create, :update]
 
   PASS_SCORE = 85
 
@@ -34,8 +32,12 @@ class HistoryTest < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_question
+    if current_question.nil?
+      self.current_question = test.questions.first if test.present?
+    else
+      self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 
   def correct_answer?(answer_ids)
@@ -45,10 +47,5 @@ class HistoryTest < ApplicationRecord
   def correct_answers
     current_question.answers.correct
   end
-
-  def before_update_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
-  end
-
 end
 
