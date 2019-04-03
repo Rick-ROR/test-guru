7951f70fe568e414169c93b01ef7a3be3adfc844
Rick-ROR <Rick.ROR@ya.ru>
2019-04-03 20:38:38 +0000

    added admin & management resource test transferred to admin

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,45 +1,12 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :get_test, only: %i[show edit update destroy start]
+  before_action :get_test, only: %i[start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_quest_404
 
   def index
     @tests = Test.all
-  end
-
-  def show
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.create(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -55,9 +22,5 @@ class TestsController < ApplicationController
 
   def get_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: { sign_up: :signup, sign_in: :login, sign_out: :logout }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
@@ -20,4 +20,9 @@ Rails.application.routes.draw do
       get :result
     end
   end
+
+  namespace :admin do
+    resources :tests
+  end
+
 end
