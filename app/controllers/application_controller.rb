e274c6e71c7404e233be52bffa021b41c078f966
Rick-ROR <Rick.ROR@ya.ru>
2019-04-04 00:34:19 +0000

    small change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    flash[:notice] = "Привет, #{current_user.name}!"
-    current_user.admin? ? admin_tests_path : super
+    flash[:notice] = "Привет, #{resource.name}!"
+    resource.admin? ? admin_tests_path : super
   end
 end
