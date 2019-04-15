01091b65c61084f315e2a7f0c3194c922d3615f6
Rick-ROR <Rick.ROR@ya.ru>
2019-04-15 20:10:51 +0000

    implemented form-inline fot index tests

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,11 +1,11 @@
 class Admin::TestsController < Admin::BaseController
-  
-  before_action :get_test, only: %i[show edit update destroy start]
+
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[show edit update destroy start update_inline]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_quest_404
 
   def index
-    @tests = Test.all
   end
 
   def show
@@ -36,6 +36,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
@@ -52,8 +60,12 @@ class Admin::TestsController < Admin::BaseController
     render plain: '404. Этот тест мы ещё не успели придумать.', status: 404
   end
 
-  def get_test
+  def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_tests
+    @tests = Test.all
   end
 
   def test_params
