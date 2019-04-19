e3101a932d83f4461098a4fd08ca5632188c177f
Rick-ROR <Rick.ROR@ya.ru>
2019-04-19 21:09:37 +0000

    migration to PG for Heroku; replace symbol ` in pswd_confirm.js

--- app/assets/javascripts/utilities/pswd_confirm.js
+++ app/assets/javascripts/utilities/pswd_confirm.js
@@ -12,13 +12,13 @@ function validatePasswordFields() {
     var password_confirmation = document.querySelector('#user_password_confirmation');
 
     if (password_confirmation.value == '' || password.value == '') {
-        document.querySelector(`.alert-danger`).classList.add('hide')
-        document.querySelector(`.alert-success`).classList.add('hide')
+        document.querySelector('.alert-danger').classList.add('hide')
+        document.querySelector('.alert-success').classList.add('hide')
     } else if (password.value == password_confirmation.value) {
-        document.querySelector(`.alert-danger`).classList.add('hide')
-        document.querySelector(`.alert-success`).classList.remove('hide')
+        document.querySelector('.alert-danger').classList.add('hide')
+        document.querySelector('.alert-success').classList.remove('hide')
     } else {
-        document.querySelector(`.alert-danger`).classList.remove('hide')
-        document.querySelector(`.alert-success`).classList.add('hide')
+        document.querySelector('.alert-danger').classList.remove('hide')
+        document.querySelector('.alert-success').classList.add('hide')
     }
 }
